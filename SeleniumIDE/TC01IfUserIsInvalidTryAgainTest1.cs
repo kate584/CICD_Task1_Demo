c3b2aad415050cca8759c02a1410780413ca72f1
kate584 <kativ2024@abv.bg>
2025-04-08 16:39:46 +0300

    Configure chrome to work on remote machine

--- SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
+++ SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
@@ -21,7 +21,20 @@ public class TC01IfUserIsInvalidTryAgainTest
     [SetUp]
     public void SetUp()
     {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.AddArgument("headless");
+        options.AddArgument("no-sandbox");
+        options.AddArgument("disable-gpu");
+        options.AddArgument("disable-dev-shm-usage");
+
+        string usingDataDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+
+        Directory.CreateDirectory(usingDataDir);
+
+        options.AddArgument($"--user-data-dir={usingDataDir}");
+
+
+        driver = new ChromeDriver(options);
         js = (IJavaScriptExecutor)driver;
         vars = new Dictionary<string, object>();
     }
